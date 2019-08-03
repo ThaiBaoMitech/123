791583a2d2be4b8f77a6315e103f2e71814352ab
ThaiBaoMitech <53608052+ThaiBaoMitech@users.noreply.github.com>
2019-08-03 21:40:02 +0700

    Add project files.

---
+++ LangugeFeatures/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LangugeFeatures.Models;
+
+namespace LangugeFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+        public ActionResult AutoProperty()
+        {
+            Product myproduct = new Product();
+
+            myproduct.Name = "Kayak";
+
+            string productName = myproduct.Name;
+
+            return View("Resual",(object)String.Format("ProductName: {0}",productName));
+        }
+    }
+}
---
+++ LangugeFeatures/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LangugeFeatures.Models
+{
+    public class Product
+    {
+        private string name;
+
+        public object ProductID { get; set; }
+        public string Name
+        {
+            get
+            {
+                return ProductID + name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
